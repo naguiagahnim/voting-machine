1b70b341f6ddaaa625288b23a58cbc87efa8736a
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2025-01-24 16:49:58 +0100

    multi arguments

--- src/configuration.rs
+++ src/configuration.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Configuration {
-    #[arg(short = 'c', long, value_delimiter = ',')]
+    #[arg(short = 'c', long, value_delimiter = ',', num_args = 1..)]
     pub candidates: Vec<String>,
 }
 

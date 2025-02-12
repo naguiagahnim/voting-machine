006775a7c14af2b40a0d81de8cd813f6c15e1f21
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2025-02-12 14:15:17 +0100

    turns out j'avais pas besoin de Arc Mutex

--- src/storages/memory.rs
+++ src/storages/memory.rs
@@ -1,41 +1,33 @@
 use crate::{domain::*, storage::Storage};
 use async_trait::async_trait;
-use std::sync::{Arc, Mutex};
 use anyhow::Result;
 
 pub struct Memory {
-    machine: Arc<Mutex<VotingMachine>>
+    machine: VotingMachine
 }
 
 #[async_trait]
 impl Storage for Memory {
     async fn new(machine: VotingMachine) -> Result<Self> {
         Ok(Self { 
-            machine: Arc::new(Mutex::new(machine))
+            machine
         })
     }
 
     async fn get_voting_machine(&self) -> Result<VotingMachine> {
-        let locked_machine = self.machine.lock()
-            .map_err(|_| anyhow::anyhow!("Failed to acquire lock"))?;
-        let candidates = locked_machine.get_scoreboard().scores.keys()
-            .map(|c| Candidate(c.0.clone()))
-            .collect();
-        let new_scoreboard = Scoreboard::new(candidates);
-        
-        Ok(VotingMachine::new(new_scoreboard))
+        Ok(self.machine.clone())
     }
 
     async fn put_voting_machine(&mut self, machine: VotingMachine) -> Result<()> {
-        let mut locked_machine = self.machine.lock()
-            .map_err(|_| anyhow::anyhow!("Failed to acquire lock"))?;
-        *locked_machine = machine;
+        self.machine = machine;
         Ok(())
     }
 }
 
+#[cfg(test)]
 mod tests {
     use super::*;
+    use tokio;
 
     fn setup() -> VotingMachine {
         let candidates = vec![
